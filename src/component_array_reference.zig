8f92c376b527c5d46007b069b869e5959fe65f2e
PegaFox <pegafox@protonmail.com>
2025-09-14 12:14:34 -0400

    fix ComponentArrayReference function pointers to be const

--- src/component_array_reference.zig
+++ src/component_array_reference.zig
@@ -10,8 +10,8 @@ const mainspace = @import("root.zig");
 
 componentArray: *anyopaque,
 deinit: *const fn (ptr: *anyopaque, allocator: Allocator) void,
-typeSize: *fn () usize,
-count: *fn () usize,
+typeSize: *const fn () usize,
+count: *const fn () usize,
 setAtID: *const fn (ptr: *anyopaque, allocator: Allocator, id: mainspace.Entity, value: *const anyopaque) void,
 
 pub fn init(comptime Type: type, allocator: Allocator) !Self
